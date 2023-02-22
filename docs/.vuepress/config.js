002b38aecd1682e9b72c8e63e303214f1fcb28b7
Sugarscat <tsugarcat@gmail.com>
2023-02-22 18:38:17 +0800

    new project

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,32 @@
+import { defineUserConfig } from 'vuepress'
+import { backToTopPlugin } from '@vuepress/plugin-back-to-top'
+import { defaultTheme } from 'vuepress'
+export default defineUserConfig({
+    base: '/image-processing/',
+    publicPath: '/image-processing/',
+    lang: 'zh-CN',
+    title: 'image-processing',
+    description: '文档。',
+    head: [['link', { rel: 'icon', href: 'favicon.ico' }]],
+    plugins: [
+        backToTopPlugin(),
+    ],
+    markdown: {
+        headers: {
+            level: [2, 3, 4, 5],
+        },
+    },
+    theme: defaultTheme({
+        // 导航栏
+        navbar: [
+            {
+                text: '首页',
+                link: '/',
+            },
+            {
+                text: 'Github',
+                link: 'https://github.com/Sugarscat/',
+            },
+        ],
+    }),
+})
